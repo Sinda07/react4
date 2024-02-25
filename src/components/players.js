22049e3ac5165d65aa5b7ad8dcbae76ead5ae8e8
Sinda07 <guesmisinda123@gmail.com>
2024-02-25 20:39:21 +0100

    players

---
+++ src/components/players.js
@@ -0,0 +1,40 @@
+const players = [
+  {
+    name: "Lionel Messi",
+    team: "Paris Saint-Germain",
+    nationality: "Argentina",
+    jerseyNumber: 30,
+    age: 34,
+    imageUrl:
+      "https://cdn.britannica.com/35/238335-050-2CB2EB8A/Lionel-Messi-Argentina-Netherlands-World-Cup-Qatar-2022.jpg",
+  },
+  {
+    name: "Kylian Mbappé",
+    team: "Paris Saint-Germain",
+    nationality: "Français",
+    jerseyNumber: 7,
+    age: 25,
+    imageUrl:
+      "https://a2.espncdn.com/combiner/i?img=%2Fphoto%2F2023%2F0406%2Fr1155161_1296x729_16%2D9.jpg",
+  },
+  {
+    name: "Neymar",
+    team: "AL Hilal",
+    nationality: " Brazilian",
+    jerseyNumber: 10,
+    age: 32,
+    imageUrl:
+      "https://www.parismatch.com/lmnr/r/960,640,FFFFFF,forcex,center-middle/img/var/pm/public/styles/paysage/public/media/image/2023/12/22/15/gettyimages-1245407658.jpg?VersionId=w49TNYh8Xh9MOP36u_4tWTB.HaFCQxUN",
+  },
+  {
+    name: "Harry Kane",
+    team: "England national team",
+    nationality: "English",
+    jerseyNumber: 9,
+    age: 30,
+    imageUrl:
+      "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blte12d25a24d4a79d7/64be5e6e34a8da44bbeff6d9/GettyImages-1482432598.jpg?auto=webp&format=pjpg&width=3840&quality=60",
+  },
+];
+
+export default players;
